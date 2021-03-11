3d3fa25c56323c869506717cef66a917ee531a85
Radu Ilina <ilinaraducristian@protonmail.com>
2021-03-11 09:10:13 +0200

    added api specification, database configuration for testing, swagger bundler and openapi generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "2.4.0"
-  id("io.spring.dependency-management") version "1.0.10.RELEASE"
-  idea
-  kotlin("jvm") version "1.4.10"
-  kotlin("plugin.spring") version "1.4.10"
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.openapi.generator") version "5.0.1"
+    idea
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
 }
 
 group = "org.moeawebframework"
@@ -13,52 +14,80 @@ version = "3.0"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-  implementation("org.springframework.boot:spring-boot-starter-amqp")
-  implementation("org.springframework.boot:spring-boot-starter-rsocket")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-rsocket")
 
-  implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-  implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 
-  runtimeOnly("com.h2database:h2")
-  runtimeOnly("org.postgresql:postgresql")
-  implementation("io.r2dbc:r2dbc-h2")
-  implementation("io.r2dbc:r2dbc-postgresql")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("io.r2dbc:r2dbc-h2")
+    implementation("io.r2dbc:r2dbc-postgresql")
 
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-test") {
-    exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-  }
-  testImplementation("com.github.kstyrc:embedded-redis:0.6")
-  testImplementation("io.projectreactor:reactor-test")
-  testImplementation("org.springframework.amqp:spring-rabbit-test")
-  testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("com.github.kstyrc:embedded-redis:0.6")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.amqp:spring-rabbit-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    freeCompilerArgs = listOf("-Xjsr305=strict")
-    jvmTarget = "11"
-  }
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.openApiGenerate {
+    validateSpec.set(false)
+    generatorName.set("kotlin-spring")
+    outputDir.set("$buildDir/openapi")
+    inputSpec.set("$buildDir/api-spec.yml")
+    packageName.set("org.moeawebframework")
+    apiPackage.set("${packageName.get()}.controllers")
+    additionalProperties.put("basePackage", packageName.get())
+    modelPackage.set("${packageName.get()}.models")
+    additionalProperties.put("artifactId", "MOEAWebFramework")
+    additionalProperties.put("gradleBuildFile", "false")
+    additionalProperties.put("reactive", "true")
+    additionalProperties.put("serializationLibrary", "jackson")
+    additionalProperties.put("title", "MOEA Web Framework")
+}
+
+tasks.register<Exec>("compileSpec") {
+    commandLine("swagger-cli", "bundle", "-r", "-o", "${buildDir}/api-spec.yml", "-t", "yaml", "${rootDir}/spec/api.yml")
+}
+
+afterEvaluate {
+    if (tasks.findByName("compileSpec") != null) {
+        tasks.openApiGenerate {
+            dependsOn("compileSpec")
+        }
+    }
 }
